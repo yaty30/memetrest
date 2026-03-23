25d55c414de30ab983fd5f73308174841fd0cf72
yaty30 <josueyat318@gmail.com>
2026-03-23 18:19:33 +0800

    feat: add SVG Logo component
    - Create Logo.tsx with proper viewBox for scalable rendering
    - Add icon.svg asset
    - Update NavBrand to use new Logo component

--- src/components/navigation/NavBrand.tsx
+++ src/components/navigation/NavBrand.tsx
@@ -1,5 +1,6 @@
 import { Stack, Typography } from "@mui/material";
 import iconSrc from "../../assets/icon.png";
+import Logo from "../Logo";
 
 export default function NavBrand() {
   return (
@@ -9,20 +10,14 @@ export default function NavBrand() {
       spacing={1}
       sx={{ flexShrink: 0 }}
     >
-      <img
-        src={iconSrc}
-        alt="Memetrest logo"
-        width={28}
-        height={28}
-        style={{ objectFit: "contain" }}
-      />
+      <Logo />
       <Typography
         variant="h6"
         sx={{
           fontFamily: "'Indie Flower', cursive",
           fontWeight: 700,
           fontSize: "1.2rem",
-          color: "#1A1625",
+          color: "text.primary",
           lineHeight: 1,
           userSelect: "none",
           letterSpacing: "-0.01em",
