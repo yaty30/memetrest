4a3ef848e36e6c015b95d8266f51dc2d8ba846e4
James <josueyat318@gmail.com>
2026-03-29 05:10:04 +0800

    feat: add Redux store with currentUserProfile slice
    - Add @reduxjs/toolkit and react-redux dependencies
    - Create currentUserProfileSlice with hydration lifecycle actions
    - Wire ReduxProvider into main.tsx app root

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,23 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Provider as ReduxProvider } from "react-redux";
 import { AppThemeProvider } from "./providers/ThemeProvider";
 import { AuthProvider } from "./providers/AuthProvider";
+import { store } from "./store";
 import "./index.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <AppThemeProvider>
-        <AuthProvider>
-          <App />
-        </AuthProvider>
-      </AppThemeProvider>
-    </BrowserRouter>
+    <ReduxProvider store={store}>
+      <BrowserRouter>
+        <AppThemeProvider>
+          <AuthProvider>
+            <App />
+          </AuthProvider>
+        </AppThemeProvider>
+      </BrowserRouter>
+    </ReduxProvider>
   </StrictMode>,
 );
